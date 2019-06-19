ce3069aebc07652e15768f11e76f7f0c6c4781b2
Hueston94 <30508835+Hueston94@users.noreply.github.com>
2019-06-19 23:28:06 +0100

    Add files via upload

---
+++ TypeWithCodePythonRandomURL.js
@@ -0,0 +1,11 @@
+javascript:(function(){var links = [
+    "https://type.withcode.uk/python/",
+    "https://type.withcode.uk/python/Variables_and_Constants",
+    "https://type.withcode.uk/python/String_manipulation",
+    "https://type.withcode.uk/python/Builtin_functions",
+    "https://type.withcode.uk/python/Input_and_output",
+    "https://type.withcode.uk/python/Arrays",
+    "https://type.withcode.uk/python/Conditional_logic",
+    "https://type.withcode.uk/python/Repetition_Iteration",
+    "https://type.withcode.uk/python/Functions_Procedures" ];
+function getRandomInt(min, max) { return Math.floor(Math.random() * (max - min + 1) + min); } window.location.href = links[getRandomInt(0, links.length-1)]; })();
